504e9fc7eab8d22f7f1b3960d870bacafa6cf279
Diogo Filipe Diogo Filipe Toscano Lourinho Gomes <ddiogo-f@c2r7s8.42lisboa.com>
2025-04-21 12:49:30 +0100

    executor agora checka parsing. Parsing nao esta a apanhar args depois de um arg entre aspas. De resto, fantastico trabalho

--- minishell/src/pipes.c
+++ minishell/src/pipes.c
@@ -52,6 +52,47 @@ void	executor_fork(t_command *cmds, t_shell *shell)
 
 void	executor(t_command *cmds, t_shell *shell)
 {
+	// echo(ou outro) arg1 "arg2" arg3
+	// arg3 nao esta a aparecer. argumentos depois de um argumento em "" ja nao estao
+	// a entrar 
+	int	i;
+	int j;
+
+	i = 1;
+	while (cmds && shell)
+	{
+		ft_putnbr_fd(i, 1);
+		ft_putendl_fd("o comando:", 1);
+		j = 0;
+		while (cmds->args[j])
+		{
+			write(1, "       *", 8);
+			write(1, cmds->args[j], ft_strlen(cmds->args[j]));
+			ft_putendl_fd("*", 1);
+			j++;
+		}
+		if (cmds->path)
+		{
+			ft_putendl_fd("    no path:", 1);
+			write(1, "       *", 8);
+			write(1, cmds->path, ft_strlen(cmds->path));
+			ft_putendl_fd("*", 1);
+		}
+		while (cmds->redir)
+		{
+			ft_putendl_fd("    com redir:", 1);
+			write(1, "       *", 8);
+			write(1, cmds->redir->direction, ft_strlen(cmds->redir->direction));
+			ft_putendl_fd("*", 1);
+			write(1, "       *", 8);
+			write(1, cmds->redir->passorfile, ft_strlen(cmds->redir->passorfile));
+			ft_putendl_fd("*", 1);
+			cmds->redir = cmds->redir->next;
+		}
+		i++;
+		cmds = cmds->next;
+	}
+	/*
 	if (cmds->next == NULL && is_builtin(cmds->args[0]))
 	{
 		redirector(cmds->redir, 0);
@@ -61,5 +102,7 @@ void	executor(t_command *cmds, t_shell *shell)
 	{
 		ft_printf("\n\n\n\nola\n\n\n\n");
 		executor_fork(cmds, shell);
-	}//Aqui devia limpar tudo ? qd sai tem que passar por aqui, devia limpar aqui tudo.
+	}
+	*/
+	//Aqui devia limpar tudo ? qd sai tem que passar por aqui, devia limpar aqui tudo.
 }
