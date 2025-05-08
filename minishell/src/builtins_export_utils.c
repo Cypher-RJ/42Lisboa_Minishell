02cc28f63c7957e69a7c66f2c30a622099e1b410
Diogo Filipe Diogo Filipe Toscano Lourinho Gomes <ddiogo-f@c1r11s2.42lisboa.com>
2025-05-08 09:48:31 +0100

    test it

--- minishell/src/builtins_export_utils.c
+++ minishell/src/builtins_export_utils.c
@@ -8,11 +8,18 @@ int	is_var_valid(char *str)
 	while (str[i] && ft_isalnum(str[i]))
 		i++;
 	if ((str[i] != '=' && str[i] != '\0') || !ft_isalpha(str[0]))
-    {
-        ft_putstr_fd("minishell: export: `", STDERR_FILENO);
-        ft_putstr_fd(str, STDERR_FILENO);
-        ft_putstr_fd("': not a valid identifier", STDERR_FILENO);
+	{
+		ft_putstr_fd("minishell: export: `", STDERR_FILENO);
+		ft_putstr_fd(str, STDERR_FILENO);
+		ft_putstr_fd("': not a valid identifier", STDERR_FILENO);
 		return (0);
-    }
-    return (1);
+	}
+	return (1);
+}
+
+int	export_var(char *str, t_shell *shell, bool has_fork)
+{
+	if (str && shell && has_fork)
+		return (1);
+	return (0);
 }
