56cae7d56f1b3017ea8e90edc2f00eb5963c25a1
Diogo Filipe Diogo Filipe Toscano Lourinho Gomes <ddiogo-f@c1r10s8.42lisboa.com>
2025-05-26 11:25:52 +0100

    heredoc faz expansoes. Posso ter problemas com expansoes que nao vao ao envp

--- minishell/src/heredoc_expansion.c
+++ minishell/src/heredoc_expansion.c
@@ -1,11 +1,32 @@
 #include "../includes/minishell.h"
 
-int	put_line_fd(int fd, int pos, char *line)
+int	put_line_hd(int fd, int pos, char *line)
 {
-	while (line[pos] != 0 || line[pos] != '$')
+	while (line[pos] != 0 && line[pos] != '$')
 	{
 		ft_putchar_fd(line[pos], fd);
 		pos++;
+	}
+	return (pos);
+}
+
+int	put_expansion_hd(int fd, int pos, char *line, t_shell *shell)
+{
+	int	i;//posicao final da expansao
+	int j;
+
+	i = pos;
+	j = 0;
+	if (!ft_isalpha(line[pos]))
+		return (ft_putchar_fd(line[pos - 1], fd), pos);
+	while (line[i] && ft_isalnum(line[i]))
+		i++;
+	while (shell->envp[j])
+	{
+		if (!ft_strncmp(&line[pos], shell->envp[j], i - pos) && \
+			shell->envp[j][i - pos] == '=')
+			return (ft_putstr_fd(&shell->envp[j][i - pos + 1], fd), i);
+		j++;
 	}
 	return (pos);
 }
@@ -17,8 +38,9 @@ void	capture_heredoc(char *line, int fd, t_shell *shell)
 	i = 0;
 	while (line[i] != 0)
 	{
-		i = put_line_fd(fd, i, line);
+		i = put_line_hd(fd, i, line);
 		if (line[i] == '$')
-			i = put_expansion_fd(fd, i, line, shell);
+			i = put_expansion_hd(fd, i + 1, line, shell);
 	}
+	ft_putchar_fd('\n', fd);
 }
